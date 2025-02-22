20b12dfa6a41aa15ea3c1c4714cab041894e8143
Rector24 <rectorphakula24@gmail.com>
2025-02-22 09:00:17 +0200

    Add files via upload

---
+++ Programming project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string Fname = Console.ReadLine();
+
+            bool residenceStudent = true;
+            bool NotResidenceStudent = false;
+            Console.WriteLine("Are you a Residence student?");
+            bool residenceStudentOrNot = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many years have you been at campus?");
+            int YearsAtCampus = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many years have you been a residence?");
+            int YearsAtCurrentResidence = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your monthly allowance/salary?");
+            decimal MonthlySalary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("what is your average mark across all subjects(%)?");
+            double percentage = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
